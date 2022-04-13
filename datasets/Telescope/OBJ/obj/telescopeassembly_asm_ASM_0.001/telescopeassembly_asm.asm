9c4ee002848966977a120fa8fe17782d09befca3
Romero Victor <romero.victor@outlook.fr>
2022-04-13 14:31:30 +0200

    Adding Neo4j database and OBJ
    Adding data for the telescope dataset

---
+++ datasets/Telescope/OBJ/obj/telescopeassembly_asm_ASM_0.001/telescopeassembly_asm.asm
@@ -0,0 +1,44 @@
+# step2vr assembly file: /var/www/stepvrweb/fichiers/TelescopeVersionCreo/telescopeassembly_asm.stp basic components extraction 
+# 
+component 1-1
+component-name 1-1 1-1--SUPPORT
+component-real-name 1-1 SUPPORT
+component-type 1-1 Component
+component-transform 1-1 ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,0,1))
+component-filename 1-1 1-1--SUPPORT
+component-father 1-1 1
+component-center 1-1 -3.5832e-19 0.059892 -0.0181106
+component-axis1 1-1 -3.09065e-16 0.547096 0.83707
+component-axis2 1-1 1 1.93437e-16 2.42795e-16
+component-axis3 1-1 -2.90886e-17 0.83707 -0.547096
+component-mass 1-1 0.00300506
+component-inertia 1-1 3.6362e-05 3.54332e-05 1.84058e-05
+component-radius 1-1 0.110001 0.108587 0.0782621
+component-bounding-box 1-1 -0.135299 -1e-07 -0.135299 0.135299 0.352472 0.135299
+
+component 1-2
+component-name 1-2 1-2--OPTICALSYSTEM
+component-real-name 1-2 OPTICALSYSTEM
+component-type 1-2 Component
+component-transform 1-2 ((-0.0211844,0,-0.999776,0),(0.999776,0,-0.0211844,0.32),(0,-1,0,0.012),(0,0,0,1))
+component-filename 1-2 1-2--OPTICALSYSTEM
+component-father 1-2 1
+component-center 1-2 6.41541e-11 0.000469013 9.33796e-13
+component-axis1 1-2 1 7.93458e-09 4.0459e-12
+component-axis2 1-2 -7.93458e-09 1 1.12823e-12
+component-axis3 1-2 -4.0459e-12 -1.12823e-12 1
+component-mass 1-2 0.00455731
+component-inertia 1-2 6.45334e-05 6.44058e-05 8.28776e-06
+component-radius 1-2 0.118998 0.11888 0.0426446
+component-bounding-box 1-2 -0.0649436 -0.0649436 -0.2 0.0649436 0.0700001 0.2
+
+component 1
+component-name 1 1--TELESCOPEASSEMBLY_ASM
+component-real-name 1 TELESCOPEASSEMBLY_ASM
+component-type 1 Assembly
+component-transform 1 ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,0,1))
+component-filename 1 1--TELESCOPEASSEMBLY_ASM
+component-father 1 None
+
+
+#enfoffile
